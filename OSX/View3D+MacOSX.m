8a79a24d87eb2504db4f05d7d5cf05e0ae992307
Ante Radman <ante@radoid.com>
2015-01-18 14:35:11 +0100

    CVDisplayLink deployed

--- OSX/View3D+MacOSX.m
+++ OSX/View3D+MacOSX.m
@@ -14,10 +14,8 @@
 	NSMutableArray *_controllerStack;
 	Controller3D *_controller;
 
-	CGRect frame;
 	Color2D color;
-	NSTimer *timer;
-	//CVDisplayLinkRef displayLink;
+	CVDisplayLinkRef displayLink;
 	BOOL _initialized, _started, _redrawing;
 }
 @synthesize color;
@@ -26,51 +24,54 @@
 + (NSOpenGLPixelFormat *)defaultPixelFormat {
 	return [[NSOpenGLPixelFormat alloc] initWithAttributes:(NSOpenGLPixelFormatAttribute[]) {
 		NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
-		NSOpenGLPFAColorSize    , 24                           ,
-		NSOpenGLPFAAlphaSize    , 8                            ,
-		NSOpenGLPFADepthSize,	32,
-		NSOpenGLPFADoubleBuffer ,
-		NSOpenGLPFAAccelerated  ,
-		NSOpenGLPFANoRecovery   ,
-		0
-/*		//NSOpenGLPFAWindow,//NSOpenGLPFAFullScreen,
-		//NSOpenGLPFADoubleBuffer,
 		NSOpenGLPFAColorSize, 24,
 		NSOpenGLPFAAlphaSize, 8,
 		NSOpenGLPFADepthSize, 32,
-		//NSOpenGLPFANoRecovery,
-		//NSOpenGLPFAAccelerated,
+		NSOpenGLPFADoubleBuffer,
+		NSOpenGLPFAAccelerated,
+		NSOpenGLPFANoRecovery,
+		0
+/*		//NSOpenGLPFAWindow,//NSOpenGLPFAFullScreen,
+		//NSOpenGLPFADoubleBuffer,
 		//NSOpenGLPFAStencilSize, 8,
 		//NSOpenGLPFAAccumSize, 0,
 		0*/}];
 }
 
-- (void)awakeFromNib {
+static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp* now, const CVTimeStamp* outputTime, CVOptionFlags flagsIn, CVOptionFlags* flagsOut, void* displayLinkContext) {
+	CVReturn result = [(__bridge View3D *)displayLinkContext getFrameForTime:outputTime];
+	return result;
+}
 
+- (CVReturn)getFrameForTime:(const CVTimeStamp*)outputTime {
+	if (_redrawing) {
+		[self.openGLContext makeCurrentContext];
+		[self draw];
+	}
+	return kCVReturnSuccess;
 }
 
 - (id)initWithFrame:(NSRect)initframe {
 	if ((self = [super initWithFrame:initframe])) {
-		frame = NSRectToCGRect(initframe);
 		color = Color2DMake(.8, .8, .8, 1);
 		_initialized = NO;
-		//[[self openGLContext] makeCurrentContext];
 	}
 	return self;
 }
 
 - (id)initWithCoder:(NSCoder*)coder {
 	if ((self = [super initWithCoder:coder])) {
-		frame = NSRectToCGRect(_frame);
-		_initialized = NO;
-		//[[self openGLContext] makeCurrentContext];
+		return [self initWithFrame:_frame];
 	}
 	return self;
 }
 
+- (void)awakeFromNib {
+	[self initWithFrame:self.frame];
+}
+
 - (void)setFrame:(NSRect)rect {
 	[super setFrame:rect];
-	//self.needsDisplay = YES;
 	[self update];
 }
 
@@ -79,8 +80,7 @@
 }
 
 - (void)reshape {
-	frame = NSRectToCGRect(_frame);
-	glViewport(0, 0, frame.size.width, frame.size.height);
+	glViewport(0, 0, _frame.size.width, _frame.size.height);
 	_redrawing = YES;
 	@synchronized (_controllerStack) {
 		[_controller reshape];
@@ -91,18 +91,20 @@
 /**
  * Drawing
  */
-
 - (void)drawRect:(NSRect)rect {
+	if (!displayLink) {
+		[self.openGLContext makeCurrentContext];
+		[self draw];
+	}
+}
+
+- (void)draw {
 	if (!_initialized) {
-		glViewport(0, 0, frame.size.width, frame.size.height);
-		glClearColor(0.6f, 0.6f, 0.6f, 1);
+		glViewport(0, 0, _frame.size.width, _frame.size.height);
+		glClearColor(color.r, color.g, color.b, 1);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		glEnable(GL_DEPTH_TEST);
 		glEnable(GL_CULL_FACE);
-		//glEnable(GL_TEXTURE_2D);
-		//glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_BLEND_SRC);
-//		glEnableClientState(GL_VERTEX_ARRAY);
-		//glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 		_initialized = YES;
 	}
 
@@ -111,16 +113,13 @@
 	[(Controller3D *)_controller draw];
 	_redrawing = NO;
 
-	//if ([self inLiveResize] && !fAnimate)
-	//	glFlush ();
-	//else
-		[[self openGLContext] flushBuffer];
+	[self.openGLContext flushBuffer];
 }
 
 - (void)setColor:(Color2D)newcolor	{
 	color = newcolor;
 	glClearColor(color.r, color.g, color.b, 1);
-	self.needsDisplay = YES;
+	self.needsDisplay = _redrawing = YES;
 }
 
 
@@ -144,11 +143,11 @@
 		_controller = newcontroller;
 		_controller.view = self;
 		//_button = nil;  // TODO
-		//if (_started) {
+		if (_initialized) {
 			[_controller start];
-			_redrawing = YES;
+			_started = _redrawing = YES;
 			self.needsDisplay = YES;
-		//}
+		}
 	}
 }
 
@@ -175,64 +174,46 @@
  */
 
 - (void)startAnimation {
-	//NSLog(@"[View3D start]");
 	if (_initialized) {
 		if (!_started)
-			[(Controller3D *)_controller start];
+			[_controller start];
 		else
-			[(Controller3D *)_controller resume];
+			[_controller resume];
 		_started = _redrawing = YES;
 	}
-	if (!timer)
-		timer = [NSTimer scheduledTimerWithTimeInterval:1/30.f target:self selector:@selector(loopAnimation) userInfo:nil repeats:YES];
-}
 
-- (void)stopAnimation {
-	//NSLog(@"[View3D stop]");
-	if (timer)
-		[timer invalidate];
-	timer = nil;
-	if (_started)
-		[(Controller3D *)_controller pause];
-}
-
-- (void)loopAnimation {
-	if (_initialized) {
-		//[[self openGLContext] makeCurrentContext];
-		if (!_started) {  // TODO ili onemogućiti da se pokrene animacija dok nije inicijaliziran GL?
-			[(Controller3D *)_controller start];
-			_started = _redrawing = YES;
-		} else
-			_redrawing |= [(Controller3D *)_controller update];
-		if (_redrawing)
-			[self drawRect:_frame];
-	}
-}
-/*
-static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp* now, const CVTimeStamp* outputTime, CVOptionFlags flagsIn, CVOptionFlags* flagsOut, void* displayLinkContext)
-{
-	[(View3D *)displayLinkContext loopAnimation];
-	return kCVReturnSuccess;
-}
-
-- (void)start {
-	NSLog(@"[View3D start]");
-	[(Controller3D *)_controller start];
-	repainting = YES;
+	[self setPixelFormat:[View3D defaultPixelFormat]]; // TODO
 
 	GLint swapInt = 1;
 	[[self openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];
-	// Create a display link capable of being used with all active displays
 	CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
-	// Set the renderer output callback function
-	CVDisplayLinkSetOutputCallback(displayLink, &MyDisplayLinkCallback, self);
-
-	// Set the display link for the current renderer
+	CVDisplayLinkSetOutputCallback(displayLink, &MyDisplayLinkCallback, (__bridge void *)self);
 	CGLContextObj cglContext = [[self openGLContext] CGLContextObj];
 	CGLPixelFormatObj cglPixelFormat = [[self pixelFormat] CGLPixelFormatObj];
 	CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext, cglPixelFormat);
 	CVDisplayLinkStart(displayLink);
-}*/
+}
+
+- (void)stopAnimation {
+	if (displayLink)
+		CVDisplayLinkRelease(displayLink);
+	displayLink = 0;
+	if (_started)
+		[(Controller3D *)_controller pause];
+}
+
+- (void)updateAnimation {
+	if (_initialized) {
+		//[[self openGLContext] makeCurrentContext];
+		if (!_started) {
+			[(Controller3D *)_controller start];
+			_started = _redrawing = YES;
+		} else
+			_redrawing |= [(Controller3D *)_controller update];
+		if (_redrawing && !displayLink)
+			[self drawRect:_frame];
+	}
+}
 
 
 /**
@@ -263,32 +244,22 @@ static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTime
 - (void)mouseDown:(NSEvent *)event {
 	Vector2D location = Vector2DMake([NSEvent mouseLocation].x - _window.frame.origin.x, [NSEvent mouseLocation].y - _window.frame.origin.y);
 	_redrawing |= [(Controller3D *) _controller touchDown:location];
-	if (_redrawing && !timer)
+	if (_redrawing && !displayLink)
 		self.needsDisplay = YES;
 }
 
 - (void)mouseUp:(NSEvent *)event {
 	Vector2D location = Vector2DMake([NSEvent mouseLocation].x - _window.frame.origin.x, [NSEvent mouseLocation].y - _window.frame.origin.y);
 	_redrawing |= [(Controller3D *) _controller touchUp:location];
-	if (_redrawing && !timer)
+	if (_redrawing && !displayLink)
 		self.needsDisplay = YES;
 }
 
 - (void)mouseDragged:(NSEvent *)event {
 	Vector2D location = Vector2DMake([NSEvent mouseLocation].x - _window.frame.origin.x, [NSEvent mouseLocation].y - _window.frame.origin.y);
 	_redrawing |= [(Controller3D *) _controller touchMove:location];
-	if (_redrawing && !timer)
+	if (_redrawing && !displayLink)
 		self.needsDisplay = YES;
-}
-
-
-/**
- * Deallocating
- */
-
-- (void)dealloc {
-    //if (displayLink)
-	//	CVDisplayLinkRelease(displayLink);
 }
 
 @end
